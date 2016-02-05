e6b50b21d930b3161dfdaf9f6b88df466907c1d8
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-05 13:37:29 +0000

    Convets 1 to I

---
+++ spec/RomanNumeralsSpec.js
@@ -0,0 +1,9 @@
+describe( "Roman Numerals Kata", function() {
+
+  describe ("Roman numeral convertion", function() {
+    it ("converts 1 to I", function() {
+      expect(convert(1)).toEqual("I");
+    });
+
+  });
+});
