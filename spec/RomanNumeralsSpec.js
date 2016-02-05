7e8bc91c61df35d563deef0473c87c142f63c89c
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-05 13:50:56 +0000

    Converts 10 to X

--- spec/RomanNumeralsSpec.js
+++ spec/RomanNumeralsSpec.js
@@ -1,13 +1,16 @@
-describe( "Roman Numerals Kata", function() {
+describe ("Roman numeral convertion", function() {
+  it ("converts 1 to I", function() {
+    expect(convert(1)).toEqual("I");
+  });
 
-  describe ("Roman numeral convertion", function() {
-    it ("converts 1 to I", function() {
-      expect(convert(1)).toEqual("I");
-    });
+  it ("converts 5 to V", function() {
+    expect(convert(5)).toEqual("V");
+  });
 
-
-    it ("converts 5 to V", function() {
-      expect(convert(5)).toEqual("V");
-    });
+  it ("converts 10 to X", function() {
+    expect(convert(10)).toEqual("X");
   });
 });
+
+
+
