bc22eb8508c2e014e9ac16ca81ec18f3b8789731
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-05 14:11:55 +0000

    Converts 3 to III

--- spec/RomanNumeralsSpec.js
+++ spec/RomanNumeralsSpec.js
@@ -8,6 +8,14 @@ describe ("Roman numeral convertion", function() {
     expect(convert(1)).toEqual("I");
   });
 
+  it ("converts 2 to II", function() {
+    expect(convert(2)).toEqual("II");
+  });
+
+  it("converts 3 to III", function() {
+    expect(convert(3)).toEqual("III");
+  });
+
   it ("converts 5 to V", function() {
     expect(convert(5)).toEqual("V");
   });
@@ -16,9 +24,6 @@ describe ("Roman numeral convertion", function() {
     expect(convert(10)).toEqual("X");
   });
 
-  it ("converts 2 to II", function() {
-    expect(convert(2)).toEqual("II");
-  });
 });
 
 
