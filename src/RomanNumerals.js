9e58a574839182704975dba56718885987fef791
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-05 14:08:42 +0000

    Refactors the lookups into a for loop

--- src/RomanNumerals.js
+++ src/RomanNumerals.js
@@ -1,14 +1,15 @@
 function convert(arabic) {
   var roman_conversion = "";
-  if (arabic == 1) {
-    roman_conversion = "I";
-  } else if (arabic == 5) {
-    roman_conversion = "V";
-  } else if (arabic == 10) {
-    roman_conversion = "X"
-  }
-  return roman_conversion;
+  var romans = ["X", "V", "I"];
+  var arabics = [10, 5, 1];
 
+  for(i = 0; i < romans.length; i++) {
+    if (arabic == arabics[i]) {
+      roman_conversion = romans[i];
+    }
+  }
+
+  return roman_conversion;
 }
 
 
